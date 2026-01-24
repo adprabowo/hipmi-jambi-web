6dcb95d7759688c05b958b54103391cccb161563
Aditya Prabowo <adityaprabowo@Adityas-MacBook-Pro.local>
2026-01-24 17:09:43 +0700

    fix: ensure sitemap is always fresh (dynamic)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,6 +3,8 @@ import { db } from "@/db";
 import { news, publications } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = 'https://bakastrahipmijambi.vercel.app'
 
